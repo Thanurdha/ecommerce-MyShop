e6eac05d0d66facf3101e8436ceebbd0367cdb88
Thanurdha <thanurdhapathirage@gmail.com>
2025-04-21 18:37:02 +0530

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import Product, Category
+
+
+def home(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'store/home.html', {
+        'products': products,
+        'categories': categories,
+    })
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, CartItem
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    return redirect('view_cart')
+@login_required
+def view_cart(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total = sum(item.subtotal() for item in cart_items)
+    return render(request, 'store/cart.html', {'cart_items': cart_items, 'total': total})
+

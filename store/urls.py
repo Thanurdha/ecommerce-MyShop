89eddb5f026476039741e2520910a2adbf3ec700
Thanurdha <thanurdhapathirage@gmail.com>
2025-04-23 02:09:52 +0530

    Save local changes before switching branches

--- store/urls.py
+++ store/urls.py
@@ -16,12 +16,10 @@ urlpatterns = [
     path('orders/', views.order_history, name='order_history'),
     path('about/', views.about, name='about'),
     path('category/<int:category_id>/', views.category_products, name='category_products'),
-<<<<<<< HEAD
     path('signup/', signup_view, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='store/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('signup/', signup_view, name='signup'),
-=======
     path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('payment/', views.payment_page, name='payment'),
     path('todays-deals/', views.todays_deals, name='todays_deals'),
@@ -29,7 +27,6 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('buy-now/<int:product_id>/', views.buy_now, name='buy_now'),
 
->>>>>>> cc23a06da6f04684da59efc7dbbf415e36726405
 ]
 
 
